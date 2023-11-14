30aaf24596607484fa9ebd862c57c5e0ac9566fa
Alcides Sandoli Neto <55538342+AlcidesSN@users.noreply.github.com>
2023-11-14 16:23:28 -0300

    Modificadores Variáveis

--- app/src/app/tab1/tab1.page.ts
+++ app/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  
+  
+  
   constructor() {}
 
+  for:number = 10;
+  des:number = 10;
+  con:number = 10;
+  int:number = 10;
+  sab:number = 10;
+  car:number = 10;
+
+
+  setfor(newfor:any){
+    this.for = newfor.target!.value;
+  }
+  setdes(newdes:any){
+    this.des = newdes.target!.value;
+  }
+  setcon(newcon:any){
+    this.con = newcon.target!.value;
+  }
+  setint(newint:any){
+    this.int = newint.target!.value;
+  }
+  setsab(newsab:any){
+    this.sab = newsab.target!.value;
+  }
+  setcar(newfor:any){
+    this.car = newfor.target!.value;
+  }
+
+
+  modificadorCalc(att:number): number{
+    return Math.floor((att-10)/2);
+  }
 }
