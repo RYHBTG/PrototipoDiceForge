c7073f1609ebc98e9f77330f763cb4dff8edbd9d
Alcides Sandoli Neto <55538342+AlcidesSN@users.noreply.github.com>
2023-11-26 22:18:57 -0300

    Adicionar e Excluir Magias

--- app/src/app/services/listas.service.ts
+++ app/src/app/services/listas.service.ts
@@ -6,13 +6,16 @@ import { Injectable } from '@angular/core';
 export class ListasService {
 
   constructor() { }
-  private listmagias:any[] = [{id: 1, name: "Mãos Flamejantes", conjuracao: "1 Ação", duracao: "Instantaneo",componentes:"V,S,M", circulo: 1 ,descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quod cupiditate voluptatem laboriosam iusto repudiandae officiis numquam magnam? Nisi debitis enim tempore cum harum similique magnam, vero laudantium repudiandae facere!" },
-                              {id: 2, name: "Misseis Magicos", conjuracao: "1 Ação", duracao: "Instantaneo", componentes: "V,S", circulo: 1, descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quod cupiditate voluptatem laboriosam iusto repudiandae officiis numquam magnam? Nisi debitis enim tempore cum harum similique magnam, vero laudantium repudiandae facere!"}];
+  private listmagias:any = [{name: "Mãos Flamejantes", conjuracao: "1 Ação", duracao: "Instantaneo",componentes:"V,S,M", circulo: 1 ,descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quod cupiditate voluptatem laboriosam iusto repudiandae officiis numquam magnam? Nisi debitis enim tempore cum harum similique magnam, vero laudantium repudiandae facere!" },
+                              {name: "Misseis Magicos", conjuracao: "1 Ação", duracao: "Instantaneo", componentes: "V,S", circulo: 1, descricao: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quod cupiditate voluptatem laboriosam iusto repudiandae officiis numquam magnam? Nisi debitis enim tempore cum harum similique magnam, vero laudantium repudiandae facere!"}];
 
   public getMagias():any[]{
     return this.listmagias;
   }
-  public addMagias(novaMagia:any){
-    this.listmagias = novaMagia;
+  public addMagias(novaMagia:any[]){
+    this.listmagias.push(novaMagia) ;
+  }
+  delMagias(indice:number){
+    this.listmagias.splice(indice,1);
   }
 }
--- app/src/app/tab4/tab4.page.ts
+++ app/src/app/tab4/tab4.page.ts
@@ -9,15 +9,16 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab4Page implements OnInit {
 
-  constructor(public listaMagias: ListasService, public alertController: AlertController) { }
+  constructor(public listaMagias: ListasService, private alertController: AlertController) { }
 
   circulos:number[] = [0,1,2,3,4,5,6,7,8,9];
 
-
+  
   ngOnInit() {
   }
   isModalOpen = false;
   
+  circuloMagia:number = 0;
   nomeMagia:string = "";
   tempoConjuracao:string = "";
   duracaoMagia:string = "";
@@ -26,13 +27,91 @@ export class Tab4Page implements OnInit {
   componenteMaterial:boolean = false;
   descricaoMagia:string = "";
 
+  limparvariaveis(){
+    this.circuloMagia = 0;
+    this.nomeMagia = "";
+    this.tempoConjuracao = "";
+    this.duracaoMagia = "";
+    this.componenteVerbal = false;
+    this.componenteSomatico = false;
+    this.componenteMaterial = false;
+    this.descricaoMagia = "";
+  }
 
-  setOpen(isOpen: boolean) {
-    if(isOpen == false){
-      
-    }
+  async setOpen(isOpen: boolean) {
+      if(isOpen == false){
+        if(!(this.nomeMagia == ""|| this.tempoConjuracao == ""|| this.duracaoMagia == ""|| !(this.componenteVerbal == false|| this.componenteSomatico == false|| this.componenteMaterial == false)|| this.descricaoMagia == ""))
+        { 
+          const componentesMagia:string = this.getComponentesMagia(this.componenteVerbal,this.componenteSomatico,this.componenteMaterial);
+          const newMagia:any = {name: this.nomeMagia, conjuracao:this.tempoConjuracao, duracao: this.duracaoMagia,componentes:componentesMagia, circulo: this.circuloMagia ,descricao:this.descricaoMagia};
+          this.listaMagias.addMagias(newMagia);
+          this.limparvariaveis();
+        }else{
+          const alert = await this.alertController.create({
+          header: 'Preecha todos os campos',
+          message: 'Algum dos campos não foi preenchido.',
+          buttons: ['Ok'],
+          });
+    
+          await alert.present();
+          return;
+        }
+      }
+    this.isModalOpen = isOpen;
+    
+  }
+  cancel(isOpen: boolean){
+    this.limparvariaveis();
 
     this.isModalOpen = isOpen;
   }
 
+  getComponentesMagia(verbal:boolean,somatico:boolean,material:boolean):string{
+
+    if(verbal && somatico && material){
+      return "V,S,M";
+    }
+    if(verbal && somatico && !material){
+      return "V,S";
+    }
+    if(verbal && !somatico && material){
+      return "V,M";
+    }
+    if(verbal && !somatico && !material){
+      return "V";
+    }
+    if(!verbal && somatico && material){
+      return "S,M";
+    }
+    if(!verbal && somatico && !material){
+      return "S";
+    }
+    if(!verbal && !somatico && material){
+      return "M";
+    }
+    if(!verbal && !somatico && !material){
+      return "";
+    }
+    return "null";
+  }
+
+  async apagarItem(index:number){
+    const alert = await this.alertController.create({
+      header: 'Excluir Magia',
+      message: 'Deseja mesmo excluir a magia',
+      buttons: [
+        {
+          text: 'cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Excluir',
+          handler: () => this.listaMagias.delMagias(index)
+        }
+      ],
+      });
+
+      await alert.present();
+    
+  }
 }
